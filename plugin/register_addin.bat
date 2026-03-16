f81b6179b04f83d80d515c9e80ca021458ebb3ac
feudal <18054661441@163.com>
2026-03-16 15:05:12 +0800

    a

--- plugin/register_addin.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ===================================
-echo 正在注册 SolidWorks 插件...
-echo ===================================
-
-:: 以管理员身份运行 regasm
-%windir%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe "%~dp0bin\Debug\net48\plugin.dll"  /codebase
-
-pause
